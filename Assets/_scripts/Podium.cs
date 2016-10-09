60efe267fc0aea4fcb379d6380249f99b22ef8f8
mehediahmed <mehediahmed@umass.edu>
2016-10-09 08:59:03 -0400

    order code

---
+++ Assets/_scripts/Podium.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Podium : MonoBehaviour {
+
+	// Use this for initialization
+	public orderManger om;
+	public int podID;
+	public GameObject[] arr;
+
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision col){
+		
+		if (col.gameObject.tag == "object") {
+			col.gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+			om.order [podID] = col.gameObject.GetComponent<ObjectID> ().objID;
+
+			Debug.Log (om.order[podID]);
+
+
+		}
+	}
+
+}
